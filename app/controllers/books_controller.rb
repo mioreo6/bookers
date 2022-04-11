9faec0ad586e74bb697050b258cccbe1050e9786
mioreo6 <102738892+mioreo6@users.noreply.github.com>
2022-04-11 15:24:15 +0000

    [error]3-12

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,20 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books'
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
