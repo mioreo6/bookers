0216442cccff42e2c424b4ba16fd4ac55d5bfa98
mioreo6 <102738892+mioreo6@users.noreply.github.com>
2022-04-12 03:45:31 +0000

    [index]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,15 +6,21 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/books'
+    redirect_to '/books/edit'
   end
 
   def index
+    @books = Book.all
   end
 
   def show
   end
 
   def edit
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
