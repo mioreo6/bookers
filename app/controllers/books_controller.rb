f10672e4851b98b00b0feeefe28dbf6cb4c26258
mioreo6 <102738892+mioreo6@users.noreply.github.com>
2022-04-17 12:45:21 +0000

    [solution]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,12 +8,14 @@ class BooksController < ApplicationController
    if @book.save
     redirect_to book_path(@book.id), notice: 'Book was successfully created.'
    else
-    render :new
+     @books = Book.all
+    render :index
    end
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
